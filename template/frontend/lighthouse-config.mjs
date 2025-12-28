251f754abaced6e53089bc389030ac5a55ba3bc5
Cyprian Zdebski <shipurjan@gmail.com>
2025-12-28 16:14:56 +0100

    improve lighthouse action

---
+++ template/frontend/lighthouse-config.mjs
@@ -0,0 +1,23 @@
+export default {
+  extends: 'lighthouse:default',
+  settings: {
+    formFactor: 'desktop',
+    throttling: {
+      rttMs: 40,
+      throughputKbps: 10 * 1024,
+      cpuSlowdownMultiplier: 1,
+      requestLatencyMs: 0,
+      downloadThroughputKbps: 0,
+      uploadThroughputKbps: 0,
+    },
+    screenEmulation: {
+      mobile: false,
+      width: 1920,
+      height: 1080,
+      deviceScaleFactor: 1,
+      disabled: false,
+    },
+    emulatedUserAgent:
+      'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36',
+  },
+}
